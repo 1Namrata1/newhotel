41076b9d686f6d18fe31280dfe2caaa9f2c05680
1Namrata1 <74266485+1Namrata1@users.noreply.github.com>
2021-07-12 12:01:28 +0530

    Add files via upload

---
+++ history.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Welcome to James Buchanan Pub and Restaurant</title>
+<link rel="stylesheet" type="text/css" href="xres/css/style.css" />
+<link rel="icon" type="image/png" href="xres/images/favicon.png" />
+<link type="text/css" href="css/styles.css" rel="stylesheet" media="all" />
+</head>
+
+<body>
+<div id="wrapper">
+	<div id="header">
+    <h1><a href="index.php"><img src="xres/images/logo.png" class="logo" alt="James Buchanan Pub and Restaurant" /></a></h1>
+        <ul id="mainnav">
+			<li><a href="index.php">Home</a></li>
+			<li><a href="gallery.php">Gallery</a></li>
+			<li class="current"><a href="history.php">History</a></li>
+			
+            <li><a href="location.php">location</a></li>
+			<li><a href="contact.php">Contact Us</a></li>
+    	</ul>
+	</div>
+    <div id="content">
+    	<div id="gallerycontainer">
+			<div class="portfolio-area" style=" padding:140px 0 20px 0;">	
+		<b>	From little boutique hotels to large chains – they can be found all around the world. Nowadays, being able to stay at the hotel is something obvious, but where did they come from? What is the history of hotels?</b></br>
+
+Today the hotel industry is experiencing a rapid growth. With the development of airlines, especially budget airlines, more people from all walks of life can afford to travel. People spend more not only on holidays but on services in general, so the entire tourist market is thriving. According to the Global Business Travel Association in 2015 “Global Business Travel Spend Topped Record-Breaking $1.2 Trillion USD”. GBTA also predicts that those numbers will reach $1.6 Trillion by 2020. But what breaking points led hoteliers up to this point?</br></br>
+
+<b>The Beginnings</b>
+<p>Let’s start from the beginning. According to the Oxford English Dictionary hotel is “An establishment providing accommodation, meals, and other services for travellers and tourists.”, but where does the word come from? Its origin goes all the way back to the Old French word hostel, later transitioned into hôtel and adapted by the Middle English.</p>
+
+But hotels as we know them today only started to appear around the 18th century, although the beginning of the hospitality industry goes all the way back to the ancient Greek and Roman culture. The lodgings were being built next to the thermal baths, in the resort towns, and on the routes popular amongst the government officials. Similar locations were popular on the routs of the caravans of the Middle East. Their purpose was nearly the same. Provide accommodation and food for the travellers of those times. Back then mostly the rich, the traders and the politicians were the main customers for the early “hoteliers”.</br></br>
+
+<b>The Middle Ages</b>
+<p>With the rise of the Christianity in the Middle Ages came the increase in the popularity of the monasteries and abbeys. Unlike their Roman counterparts, they offered a bed and a supper to all the travellers, not just the politicians and the rich. Especially those going to the religious sites could find a place for themselves inside. In those times inns started to appear all around Europe. They were not, however, what we associate the word “hotel” with today. They were inns, with small rooms, often for share, taverns, stables and houses of the owners all in one place. At first, they were lodgings mostly for the pilgrims.</p></br></br>
+
+<b>The Beginnings of the Hotels as We Know Them Today</b></br>
+<p>In 1792 the first publicly held hotel, called the City Hotel, opened in NYC. The first hotel in the present sense of the word, the Tremont House, opened in Boston in 1829. It was a quite remarkable place, that set many “firsts” for the industry. Most notably it was the first hotel with indoor plumbing and indoor toilets and baths. It was also the pioneer in terms of reception as well as everyone’s favourite – free soap. Another big milestone is the creation of the first business hotel, the Buffalo Statler, in 1908.</p>
+
+
+
+<p>One big move for the industry was selling the Intercontinental Hotels to the Japanese Seibu Holdings, Inc. by the Grand Metropolitan. It was one of the first major acquisitions that started the trend.</p>
+
+<b>Going digital</b>
+<p>What made the biggest difference, apart from the cheap flights, was the creation of the Internet. This global system of computer networks changed the World. It was the beginning of the hotel websites. Hotels could now advertise themselves and as a result, gain more visitors. Moreover, the guests can now rate their stay and either warn other travellers or persuade them to go to the certain location. It is now easy for them to find the hotel they like, in the perfect location, and even make the booking without leaving the comfort of their own house.</p>
+
+
+<b>Modern-day hotels</b>
+<p>Today, the bar for the hoteliers is set very high. It becomes increasingly more difficult to please the guests. Those often exaggerated expectations lead to the creation of new hotel types. Modern hotels are bigger, more technologically advanced and more extravagant. We can now enjoy our stay in a designer hotel, an overwater bungalow or even an ice hotel or glass igloo. Recently capsule hotels are the novelty. These tiny rooms, called capsules or pods, are perfect for the travellers seeking for a cheap stay, without excessive luxuries.<p>
+
+<p>We may actually observe a duality in the industry. With rising popularity of both budget and luxury hotels. It is the answer to the division between the travellers. While some want to save as much as possible, others like to use their holidays as an excuse to splurge on anything they want.</p>
+
+<b>The future of the hotel industry</b>
+<p>We’ve discussed the past of the hotel industry, but what could be its future? The obvious answer is automatization. Machines become a vital part of many hotels and with time we believe they will be crucial for their growth.</p>
+
+<p>Today we can already see robots as receptionists and cleaning “staff”. They can give us new towels, bring us food and greet us at the entrance. The hotel rooms themselves are also technically advanced, with voice detection devices and advanced electronics.</p>
+
+The only logical next step is the Artificial Intelligence (AI). Although it will probably never completely take over, it will be a major part of the future hotels.</br></br></br>
+
+One thing we are sure of is that the future of the hotel industry truly is bright.
+
+
+			
+			
+			  
+               	<div class="column-clear"></div>
+				
+            </div>
+			<div class="clearfix"></div>
+		
+			</a>
+        </div>
+    </div>
+    
+
+
+
+<div id="footer">
+	<h4>717.328.0011 &bull; <a href="contact-us.php">Rizal Avenue, Tuburan District, Pagadian City, Zamboanga del Sur, Philippines  </a></h4>
+	<p>Hours of Operation&nbsp;&nbsp;&bull;&nbsp;&nbsp;Hotel guest check-ins available daily 3-8 PM&nbsp;&nbsp;&bull;&nbsp;&nbsp;Serving Dinner Tuesday Evenings 5-9 PM</p>
+	<a href="index.php"><img src="xres/images/footer-logo.jpg" alt="James Buchanan Pub and Restaurant" /></a>
+	<p>&copy; Copyright 2011 James Buchanan Pub and Restaurant | All Rights Reserved <br /></p>
+</div>
+
+</div>
+</body>
+</html>
